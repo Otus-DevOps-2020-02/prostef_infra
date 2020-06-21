400a3782d26c051615bb9ec162e886324ce06162
Igor Mokeev <mokeev.ig@me.com>
2020-06-21 03:33:51 +0300

    complete main task

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,12 +3,17 @@ variable project {
 }
 variable region {
   description = "Region"
-  # Значение по умолчанию
-  default = "europe-west1"
+  default     = "europe-west1"
+}
+variable zone {
+  description = "Zone"
+  default     = "europe-west-b"
 }
 variable public_key_path {
-  # Описание переменной
   description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
 }
 variable disk_image {
   description = "Disk image"
